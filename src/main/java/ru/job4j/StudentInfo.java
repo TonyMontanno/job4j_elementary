9fb68ed037cada15f592f9f46ad59790227aa03b
Антон Юн <crazy_antonio@mail.ru>
2025-01-22 18:00:30 +0300

    Разбор первой программы по взрослому [#505151]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Anton Yun");
+        System.out.println("06.11.1989");
+    }
+}
